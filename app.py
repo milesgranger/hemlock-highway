ead1772442ac3f388887c6fb4323c48f6d9a26bd
Miles Granger <miles59923@gmail.com>
2017-05-27 11:15:49 +0200

    before switching users and payments to postgres

--- app.py
+++ app.py
@@ -11,14 +11,12 @@ from rest_api.views import api_blueprint
 
 DEBUG = os.environ.get('DEBUG', True)
 
-
 # Stripe Payment setup
 stripe_keys = {
     'secret_key': os.environ.get('STRIPE_SECRET_KEY'),
     'publishable_key': os.environ.get('STRIPE_PUBLISHABLE_KEY')
 }
 stripe.api_key = stripe_keys['secret_key']
-
 
 # Application definition
 app = Flask(import_name=__name__,
