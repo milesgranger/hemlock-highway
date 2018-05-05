f7be6525c346b41e1f655aeea77d8bea67fedafe
Miles Granger <miles59923@gmail.com>
2018-05-05 12:26:01 +0200

    DataManager load first n_bytes feature

--- hemlock_highway/data_manager/manager.py
+++ hemlock_highway/data_manager/manager.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import io
+import sys
 import boto3
+import requests
 import pandas as pd
 from hemlock_highway.settings import PROJECT_CONFIG
 
@@ -28,21 +30,51 @@ class DataManager:
         self.target_column = target_column
         self.read_args = read_args
 
-    def load(self):
+    def load(self, n_bytes=None):
         """
         Execute the load from either http or s3
+        TODO: Implement loading of n_bytes
         """
-        if self.data_endpoint.startswith('http://') or self.data_endpoint.startswith('https://'):
-            self.X = pd.read_csv(filepath_or_buffer=self.data_endpoint, **self.read_args)
+
+        data = io.BytesIO()  # Holder the raw bytes either from s3 or some http endpoint.
+
+        # Load from http
+        if any([self.data_endpoint.startswith(protocol) for protocol in ('http://', 'https://', 'tcp://')]):
+
+            # Stream from source writing by chunk size of 1mb or n_bytes
+            # if n_bytes is defined, load that and break after one iteration.
+            resp = requests.get(self.data_endpoint, stream=True)
+            if resp.ok:
+                for chunk in resp.iter_content():
+                    if chunk:
+                        data.write(chunk)
+                    if n_bytes and sys.getsizeof(data) >= n_bytes:
+                        break
+            else:
+                raise IOError(
+                    f'Unable to get data from: {self.data_endpoint}; Got: {resp.status_code} Content: {resp.content}'
+                )
+
+        # Otherwise, assumed to be s3 bucket
         else:
+
+            # Request the object
             resp = self.s3_client.get_object(Bucket=self.data_endpoint.split('/')[0],
-                                             Key='/'.join(self.data_endpoint.split('/')[1:])
+                                             Key='/'.join(self.data_endpoint.split('/')[1:]),
+                                             Range=f'bytes=0-{n_bytes or ""}'
                                              )
-            if resp['ResponseMetadata']['HTTPStatusCode'] == 200:
-                self.X = pd.read_csv(io.BytesIO(resp['Body'].read()), **self.read_args)
+
+            # Read object if request was successful or raise an error
+            if str(resp['ResponseMetadata']['HTTPStatusCode']).startswith('20'):
+                data.write(resp['Body'].read())
             else:
                 raise IOError(f'Error fetching dataset from S3: {resp}')
 
+        # Seek the start of io object and read into pandas
+        data.seek(0)
+
+        # Separate X and y
+        self.X = pd.read_csv(filepath_or_buffer=data, **self.read_args)
         self.y = self.X[self.target_column]
         self.X = self.X[[col for col in self.X.columns if col != self.target_column]]
 
--- tests/test_data_manager/test_data_manager.py
+++ tests/test_data_manager/test_data_manager.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import unittest
 import moto
 
 import botocore.exceptions
 from .utils import fake_data_on_s3
-
 
 
 class DataManagerTestCase(unittest.TestCase):
@@ -16,13 +16,13 @@ class DataManagerTestCase(unittest.TestCase):
         # Test data directory
         self.DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data')
 
-    @fake_data_on_s3(local_dataset='basic_integer_data.csv', bucket='test', key='data/basic.csv')
-    def test_basic_integer_data_load(self):
+    @fake_data_on_s3(local_dataset='iris.csv', bucket='test', key='data/basic.csv')
+    def test_load_from_s3(self):
         """
         Test the basic loading of a dataset.
         """
         from hemlock_highway.data_manager import DataManager
-        dm = DataManager(data_endpoint='test/data/basic.csv', target_column='target')
+        dm = DataManager(data_endpoint='test/data/basic.csv', target_column='species')
         self.assertFalse(dm._loaded, msg='DataManger should not load data on initialization! Reports it is loaded!')
         dm.load()
         self.assertTrue(dm._loaded, msg='After asking to load data, DataManager is reporting it is not loaded!')
@@ -34,7 +34,7 @@ class DataManagerTestCase(unittest.TestCase):
         Test IOError when trying to load a non-existant dataset.
         """
         from hemlock_highway.data_manager import DataManager
-        dm = DataManager(data_endpoint='test/data/basic.csv', target_column='target')
+        dm = DataManager(data_endpoint='test/data/basic.csv', target_column='species')
 
         # Should raise an exception when trying to load a dataset that doesn't exist.
         with self.assertRaises(botocore.exceptions.ClientError):
@@ -52,6 +52,30 @@ class DataManagerTestCase(unittest.TestCase):
         dm.load()
         self.assertTrue('petal_length' in dm.X.columns,
                         msg=f'Expected "petal_length" to be in X, but found {dm.X.columns}')
+
+    @fake_data_on_s3(local_dataset='iris.csv', bucket='test', key='data/basic.csv')
+    def test_load_first_mb_s3(self):
+        """
+        Ensure dataloader can load just the for n_bytes from s3
+        """
+        from hemlock_highway.data_manager import DataManager
+        dm = DataManager(data_endpoint='test/data/basic.csv', target_column='species')
+        dm.load(n_bytes=1024)
+        size = sys.getsizeof(dm.X)
+        self.assertLess(size, 1512, msg=f'Size of data is > 1.5mb; it is {size} bytes')
+
+    def test_load_first_mb_http(self):
+        """
+        Ensure dataloader can load just the for n_bytes from s3
+        """
+        from hemlock_highway.data_manager import DataManager
+        dm = DataManager(
+            data_endpoint='https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv',
+            target_column='species'
+        )
+        dm.load(n_bytes=1024)
+        size = sys.getsizeof(dm.X)
+        self.assertLess(size, 1512, msg=f'Size of data is > 1.5mb; it is {size} bytes')
 
 
 if __name__ == '__main__':
