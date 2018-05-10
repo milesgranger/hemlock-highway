ac43e070731809fcf30d10a84e8862933f0565fe
Miles Granger <miles59923@gmail.com>
2018-05-10 12:50:34 +0200

    add more models

--- hemlock_highway/server/user_mgmt/models/user.py
+++ hemlock_highway/server/user_mgmt/models/user.py
@@ -12,14 +12,33 @@ class User(UserMixin, db.Model):
 
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id          = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email       = db.Column(db.String(256), unique=True)
     name_first  = db.Column(db.String(256), unique=False, nullable=True)
     name_last   = db.Column(db.String(256), unique=False, nullable=True)
+    transaction = db.relationship('Transaction')
+    model       = db.relationship('MachineLearningModel')
 
 
+class MachineLearningModel(db.Model):
+
+    __tablename__ = 'ml_model'
+
+    id          = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id     = db.Column(db.Integer, db.ForeignKey(User.id))
+    name        = db.Column(db.String(256), unique=False)
+    trained     = db.Column(db.Boolean, default=False)
+    size_bytes  = db.Column(db.Integer)
 
 
+class Transaction(db.Model):
+
+    __tablename__ = 'transaction'
+
+    id          = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id     = db.Column(db.Integer, db.ForeignKey(User.id))
+    amount      = db.Column(db.Float, unique=False, nullable=False)
+    description = db.Column(db.String(1000), unique=False, nullable=False)
 
 
 class OAuth(OAuthConsumerMixin, db.Model):
